32262581b4ac9d2455b6c1c27588e86add5f69d4
Kyle Dickerson <KyleDickersonComposer@gmail.com>
2023-09-07 11:15:00 -0400

    deleted un-needed spaces in the program to test that the new github actions ci/cd changes I made are working as intened.

--- BlazorTest/BlazorTest/Program.cs
+++ BlazorTest/BlazorTest/Program.cs
@@ -1,25 +1,14 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+﻿using BlazorTest;
 using BlazorTest.Data;
-using MudBlazor.Services;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using BlazorTest;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddMudServices();
 
-
 await builder.Build().RunAsync();
-
-
-
-
