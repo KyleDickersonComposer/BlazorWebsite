b1bbc9712f4679e5050dbd892db4f6c58d663344
Kyle Dickerson <kyledickersoncomposer@gmail.com>
2024-03-01 19:20:43 -0500

    major overhaul of design and content

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,4 @@
 ﻿using BlazorTest;
-using BlazorTest.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -9,8 +8,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddSingleton<WeatherForecastService>();
-// builder.Services.AddSingleton<NavigationManager>();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
